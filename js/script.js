6aa08c79696627a5f3d579ea672c4b327e3bb89b
Emin <eminkhidirov98@gmail.com>
2024-05-16 19:20:59 +0300

    21.(д)Логические операторы

--- js/script.js
+++ js/script.js
@@ -1,35 +1,42 @@
-if (1) {
-    console.log('Ok!');
-} else {
-    console.log('Eror');
-}
+// const hamburger = 5;
+// const fries = null;
 
-
-
-
-// if (num < 49) {
-//     console.log('Eror');
-// } else if (num >100) {
-//     console.log('mnogo');
-// } else {
-//     console.log('Ok!');
+// if (hamburger && fries) {
+//     console.log('ya sit');
 // }
 
-// (num === 50) ? console.log('OK!') : console.log('Eror');
+// const hamburger = 3;
+// const fries = 1;
+// const cola =1;
 
-const num = '50';
+// console.log(hamburger === 3 && cola && fries);
 
-switch (num) {
-    case '49':
-        console.log('neverno');
-        break;
-    case '10':
-        console.log('neverno');
-        break; 
-    case '50':     
-        console.log('v tocku');
-        break; 
-    default:
-        console.log('ne v etot raz');
-        break;        
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "skskskhsjkch");
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Vse siti');
+// } else {
+//     console.log('Mi uxodim');
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola =0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola ===2 || fries === 3 && nuggets) {
+    console.log('Vse dovolni');
+} else {
+    console.log('Mi uxodim');
 }
+
+console.log(hamburger === 3 && (cola ===2 || fries === 3) && nuggets);
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log (!0);
