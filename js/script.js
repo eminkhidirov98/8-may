59febfe1f187d8bd9f2d736f42c6886ef4f31b08
Emin <eminkhidirov98@gmail.com>
2024-05-16 19:51:28 +0300

    21.Циклы

--- js/script.js
+++ js/script.js
@@ -1,42 +1,7 @@
-// const hamburger = 5;
-// const fries = null;
 
-// if (hamburger && fries) {
-//     console.log('ya sit');
-// }
+let num = 50;
 
-// const hamburger = 3;
-// const fries = 1;
-// const cola =1;
-
-// console.log(hamburger === 3 && cola && fries);
-
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && "skskskhsjkch");
-
-// if (hamburger === 3 && cola === 1 && fries) {
-//     console.log('Vse siti');
-// } else {
-//     console.log('Mi uxodim');
-// }
-
-const hamburger = 3;
-const fries = 3;
-const cola =0;
-const nuggets = 2;
-
-if (hamburger === 3 && cola ===2 || fries === 3 && nuggets) {
-    console.log('Vse dovolni');
-} else {
-    console.log('Mi uxodim');
+while (num < 55) {
+    console.log(num);
+    num++;
 }
-
-console.log(hamburger === 3 && (cola ===2 || fries === 3) && nuggets);
-
-let johnReport, alexReport, samReport, mariaReport = 'done';
-
-console.log(johnReport || alexReport || samReport || mariaReport);
-
-console.log (!0);
