dd719b2c7d7b81607beb0ffdaa2fbcc38304b670
Emin <eminkhidirov98@gmail.com>
2024-05-17 07:08:25 +0300

    24. Практика, ч2. Применяем условия и циклы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; i++) {
-        console.log(j);
-    }
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "")
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+          b = prompt("На сколько оцените его", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50)  {
+        personalMovieDB[a] = b;  
+        console.log("done");
+    }  else {
+        console.log("eror");
+        i--;
+    } 
+
+       
+          
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
