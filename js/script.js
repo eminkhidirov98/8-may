ab6f4e3037c5023c187e59582218a7e0dcb4cb5e
Emin <eminkhidirov98@gmail.com>
2024-05-18 07:02:46 +0300

    воспроизведено 25. Функции, стрелочные ф-ции (ES6)

--- js/script.js
+++ js/script.js
@@ -1,37 +1,18 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "")
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+let num = 20;
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов", ""),
-          b = prompt("На сколько оцените его", "");
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num)
+}
+showFirstMessage('Hello World!');
+console.log(num);
 
-    if (a != null && b != null && a != "" && b != "" && a.length < 50)  {
-        personalMovieDB[a] = b;  
-        console.log("done");
-    }  else {
-        console.log("eror");
-        i--;
-    } 
-
-       
-          
+function calc(a, b,) {
+    return (a + b);
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
-}
-
-console.log(personalMovieDB);
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
