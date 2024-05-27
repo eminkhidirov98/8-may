6d13782734fdd5cf5f908c29777ba8535648acc1
Emin <eminkhidirov98@gmail.com>
2024-05-27 07:13:34 +0300

    остановлено 32. Объекты, деструктуризация объектов

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
-function first() {
-    // do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+console.log(options.name);
+
+// delete options.name;
+
+console.log(options);
+
+for (let key in options) {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
 }
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done);
